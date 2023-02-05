4dad07312f6ff2c115e5cdc97f3181cc2bcd3525
Celeste <kees.storm@xs4all.nl>
2023-02-05 11:14:57 +0100

    dialogue system and Mrs Kip dialogue

---
+++ GGJ-2023/Assets/dialogue system/DialogueManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+    public GameObject dialogueBox;
+    private Queue<string> sentences;
+    
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+   public void StartDialogue (Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+
+        nameText.text= dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
+    }
+    
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        dialogueBox.SetActive(false);
+
+    }
+}
