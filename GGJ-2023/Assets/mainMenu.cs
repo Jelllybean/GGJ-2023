216a6369c9e8ad25a931d8eef4033136bf1826bb
timo kolk <timodrumt@gmail.com>
2023-02-05 10:57:38 +0100

    creddits added, menu functionality

---
+++ GGJ-2023/Assets/mainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+   public void StartGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
